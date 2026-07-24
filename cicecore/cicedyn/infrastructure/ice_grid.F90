7aa0de4111e882e221c9dd7c37d617d94020b0f7
Nicholas Szapiro <149816583+NickSzapiro-NOAA@users.noreply.github.com>
2026-07-24 12:15:46 -0400

    unused in cicecore/cicedyn/infrastructure/ice_grid.F90

--- cicecore/cicedyn/infrastructure/ice_grid.F90
+++ cicecore/cicedyn/infrastructure/ice_grid.F90
@@ -1082,8 +1082,6 @@
 #endif
 
       integer (kind=int_kind) :: &
-         i, j, iblk, &
-         ilo,ihi,jlo,jhi, &     ! beginning and end of physical domain
          fid_grid , &           ! file id for netCDF grid file
          ierr
 
@@ -2129,7 +2127,7 @@
 
       integer (kind=int_kind) :: &
          i, j, &
-         im1, im2, jm1, jm2, im3, jm3 , &  ! i & j for mom supergrid
+         im1, im2, jm1, jm2, &  ! i & j for mom supergrid
          ierr
 
       character(len=*), parameter :: subname = '(mom_dy)'
@@ -2428,18 +2426,14 @@
 
       subroutine geosgrid_nc
 
-      use ice_blocks, only: nx_block, ny_block
-      use ice_constants, only: c0, c1, &
+      use ice_constants, only: &
           field_loc_center, field_loc_NEcorner, &
           field_type_scalar, field_type_angle
-      use ice_domain_size, only: max_blocks
 #ifdef USE_NETCDF
       use netcdf
 #endif
 
       integer (kind=int_kind) :: &
-         i, j, iblk, &
-         ilo,ihi,jlo,jhi, &    ! beginning and end of physical domain
          fid_grid              ! file id for netCDF grid file
 
       logical (kind=log_kind) :: diag
@@ -2452,9 +2446,6 @@
 
       real (kind=dbl_kind), dimension(:,:), allocatable :: &
          work_g1
-
-      type (block) :: &
-         this_block            ! block information for current block
 
       integer(kind=int_kind) :: &
          varid
@@ -2775,8 +2766,7 @@
         ! extend spacing from center of grid outward.
 
         integer (kind=int_kind) :: &
-             i, j, iblk, &
-             imid, jmid, &
+             i, j, &
              center1, center2 ! array centers for expanding dx, dy
 
         real (kind=dbl_kind) :: &
@@ -3617,7 +3607,7 @@
       subroutine NElatlon
 
       use ice_constants, only: c0, c1, c1p5, c2, p5, &
-          field_loc_center, field_loc_Nface, field_loc_Eface, &
+          field_loc_Nface, field_loc_Eface, &
           field_type_scalar
 
       integer (kind=int_kind) :: &
